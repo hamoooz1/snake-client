97408043ada00fe946c784f48f71de394c83a841
Hamza Kaderi <kaderihamza4@gmail.com>
2023-10-25 20:06:53 -0700

    refactored by importing the setupInput into different file

--- play.js
+++ play.js
@@ -1,24 +1,9 @@
 const {connect} = require("./client");
-const net = require("net");
+const {setupInput} = require("./input");
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  const handleUserInput = function (data) {
-    // your code here
-    if (data === '\u0003') {
-      process.exit();
-    }
-
-  };
-
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
 
 console.log("Connecting ...");
 connect();
+
+
 setupInput();
