996a818e18d7aa80dcad76644686f0e21aed135d
Hamza Kaderi <kaderihamza4@gmail.com>
2023-10-25 19:43:37 -0700

    added the handleUserInput to check if ctr-c is pressed in order to exit function

--- play.js
+++ play.js
@@ -1,6 +1,24 @@
 const {connect} = require("./client");
 const net = require("net");
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  const handleUserInput = function (data) {
+    // your code here
+    if (data === '\u0003') {
+      process.exit();
+    }
+
+  };
+
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
 
 console.log("Connecting ...");
 connect();
+setupInput();
